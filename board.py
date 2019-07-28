44d08052a63dbf13f3cd68aa8c561266fe7435b1
Jarrett <chapmanjt@ymail.com>
2019-07-28 16:53:33 -0500

    Board update
    + Board.progress
    + Board.__str__

--- board.py
+++ board.py
@@ -4,6 +4,9 @@ import numpy as np
 class Board:
     def __init__(self):
         # self.fleet_layout = np.zeros((10, 10), int)
+        self.cols = 'abcdefghij'
+        
+        self.tracker = np.zeros((10, 10), int)
         self.fleet_layout = [
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
@@ -16,7 +19,6 @@ class Board:
             [0, 0, 0, 0, 5, 5, 5, 5, 5, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         ]
-        self.tracker = np.zeros((10, 10), int)
 
 # key
 # 0 Empty
@@ -40,3 +42,52 @@ class Board:
 # 0 0 0 0 1 0 1 0 0 0
 # 0 0 0 1 2 2 2 2 2 0
 # 0 0 0 0 0 0 0 0 0 0
+
+    def progress(self) :
+        sunkards = game_board.tracker * game_board.fleet_layout
+        flat = sunkards.flatten('C')
+        nonzero = [x for x in flat if x > 0]
+        count_hits = len(nonzero)
+        return round(count_hits / 17 * 100,1)
+
+
+    def __str__(self) :
+        #col header
+        out = '   | '
+        for a in self.cols :
+            out += a + ' | '
+            
+        out += '\n   -'
+        
+        for a in self.cols : out += '----'
+        out += '\n'
+        
+        #row index
+        k = 1
+        for i in self.tracker :
+            if k < 10 : out += ' ' + str(k) + ' | '; 
+            else : out += str(k) + ' | '
+            
+            for j in i : 
+                if str(j) == '0' : out += '  | '
+                else: 
+                    out +=  str(j) + ' | '
+            k += 1
+            out += '\n'
+            
+        
+        for a in self.cols : out += '-----'
+        out += '\n'
+        
+        k = 1
+        for i in self.fleet_layout :
+            if k < 10 : out += ' '
+            out += str(k) + ' | '
+            for j in i :
+                if str(j) == '0' : out += '  | '
+                else: 
+                    out += str(j) + ' | '
+            k += 1
+            out += '\n'
+            
+        return out
